f0010eab3c10a9c67f687d0b3fdd21c5aadbf636
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-01 18:03:39 +0200

    create area model

---
+++ src/models/area.model.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const {
+  createRequiredFieldValidator,
+} = require("../utils/fieldValidationUtils");
+
+const {
+  FIELD_CANNOT_BE_SHORTER,
+  FIELD_CANNOT_BE_LONGER,
+} = require("../consts/errors");
+
+const { MIN_NAME_LENGTH, MAX_AREA_NAME_LENGTH } =
+  require("../consts/validation").lengths;
+
+const areaSchema = new Schema({
+  name: {
+    type: String,
+    required: createRequiredFieldValidator("name"),
+    minLength: [
+      MIN_NAME_LENGTH,
+      FIELD_CANNOT_BE_SHORTER("area name", MIN_NAME_LENGTH),
+    ],
+    maxLength: [
+      MAX_AREA_NAME_LENGTH,
+      FIELD_CANNOT_BE_LONGER("area name", MAX_AREA_NAME_LENGTH),
+    ],
+  },
+});
+
+module.exports = model("Area", areaSchema);
