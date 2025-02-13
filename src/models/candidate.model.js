c70b1a496944cc34db70f72af643ed4319d03604
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-13 22:52:24 +0200

    create candidate model

---
+++ src/models/candidate.model.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+
+const { createRequired } = require("../utils/fieldValidationUtils");
+
+const { FIELD_CANNOT_BE_SHORTER } = require("../consts/errors");
+
+const { MIN_NAME_LENGTH } = require("../consts/validation").lengths;
+
+const candidateSchema = new Schema({
+  firstName: {
+    type: String,
+    required: createRequired("first name"),
+    minLength: [
+      MIN_NAME_LENGTH,
+      FIELD_CANNOT_BE_SHORTER("first name", MIN_NAME_LENGTH),
+    ],
+  },
+
+  lastName: {
+    type: String,
+    required: createRequired("last name"),
+    minLength: [
+      MIN_NAME_LENGTH,
+      FIELD_CANNOT_BE_SHORTER("last name", MIN_NAME_LENGTH),
+    ],
+  },
+
+  patronymic: {
+    type: String,
+    minLength: [
+      MIN_NAME_LENGTH,
+      FIELD_CANNOT_BE_SHORTER("patronymic", MIN_NAME_LENGTH),
+    ],
+  },
+
+  area: { type: String },
+
+  qualification: { type: String },
+
+  salary: { type: Number },
+
+  address: {
+    country: {
+      type: String,
+      required: createRequired("country"),
+    },
+    city: {
+      type: String,
+      required: createRequired("city"),
+    },
+  },
+
+  phone: {
+    type: String,
+    required: createRequired("phone"),
+  },
+});
+
+module.exports = model("Candidate", candidateSchema);
