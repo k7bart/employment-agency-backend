01d59b8041614889837ec6526f347876fff24f90
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-19 18:21:42 +0200

    add JWT_SECRET and JWT_EXPIRES_IN

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
   MONGODB_URL: process.env.MONGODB_URL,
   MONGODB_USER: process.env.MONGODB_USER,
   MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
+
+  JWT_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
 
   CLIENT_URL: process.env.CLIENT_URL,
   SERVER_URL: process.env.SERVER_URL,
