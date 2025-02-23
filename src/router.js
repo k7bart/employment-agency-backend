1bea0f76abaebdb36e471eda8d29227aa4eea868
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-23 23:02:01 +0200

    enhance router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
 
+const agreements = require("./agreements/agreements.routes");
+const areas = require("./areas/areas.routes");
+const auth = require("./auth/auth.routes");
 const candidates = require("./candidates/candidates.routes");
+const employers = require("./employers/employers.routes");
+const vacancies = require("./vacancies/vacancies.routes");
 
-router.use("/candidates", candidates);
+const { checkAuth } = require("./middlewares/auth");
+
+router.use("/agreements", checkAuth, agreements);
+router.use("/areas", checkAuth, areas);
+router.use("/auth", auth);
+router.use("/candidates", checkAuth, candidates);
+router.use("/employers", checkAuth, employers);
+router.use("/vacancies", checkAuth, vacancies);
 
 module.exports = router;
