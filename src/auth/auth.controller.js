4d7b040dfbc5b0108b30d21d3317d692e075eaf9
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-23 17:02:37 +0200

    add login and signup:
    - add auth controller
    - add auth routes
    - add auth service

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,22 @@
+const authService = require("./auth.service");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const token = await authService.login(email, password);
+
+  return res.status(200).json(token);
+};
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+
+  const token = await authService.signup(email, password);
+
+  return res.status(200).json(token);
+};
+
+module.exports = {
+  login,
+  signup,
+};
---
+++ src/auth/auth.service.js
@@ -0,0 +1,37 @@
+const tokenService = require("../token/token.service");
+const userService = require("../user/user.service");
+
+const { createError } = require("../utils/errorsUtils");
+
+const { INCORRECT_CREDENTIALS } = require("../consts/errors");
+
+const login = async (email, password) => {
+  const user = await userService.getUserByEmailWithPassword(email);
+
+  if (!user || !(await user.comparePasswords(password, user.password))) {
+    throw createError(401, INCORRECT_CREDENTIALS);
+  }
+
+  const token = tokenService.generateToken({
+    email: user.email,
+    id: user._id,
+  });
+
+  return token;
+};
+
+const signup = async (email, password) => {
+  const user = await userService.createUser(email, password);
+
+  const token = tokenService.generateToken({
+    email: user.email,
+    id: user._id,
+  });
+
+  return token;
+};
+
+module.exports = {
+  login,
+  signup,
+};
